65afaecc11b8fcef0feb68038fc10dcd3f1ad20c
hjkim <hjhjgudwls1022@gmail.com>
2024-09-11 19:59:32 +0900

    Collection Framework - ArrayList

---
+++ src/collection/array/MyArrayListV2Main.java
@@ -0,0 +1,40 @@
+package collection.array;
+
+public class MyArrayListV2Main {
+
+    public static void main(String[] args) {
+        MyArrayListV2 list = new MyArrayListV2(2);
+
+        System.out.println(list);
+        list.add("a");
+        System.out.println(list);
+        list.add("b");
+        System.out.println(list);
+        list.add("c");
+        System.out.println(list);
+        list.add("d");
+        System.out.println(list);
+        list.add("e");
+        System.out.println(list);
+        list.add("f");
+        System.out.println(list);
+        list.add("g");
+        System.out.println(list);
+        list.add("h");
+        System.out.println(list);
+        list.add("i");
+        System.out.println(list);
+        list.add("j");
+        System.out.println(list);
+        list.add("k");
+        System.out.println(list);
+        list.add("l");
+        System.out.println(list);
+        list.add("m");
+        list.add("n");
+        list.add("o");
+        list.add("p");
+        list.add("q");
+        System.out.println(list);
+    }
+}
