96d82a92598222d728160cd4fca1df5fc195fd4e
hjkim <hjhjgudwls1022@gmail.com>
2024-09-13 01:14:36 +0900

    Collection Framework - Hash(해시 알고리즘)

---
+++ src/collection/set/HashStart4.java
@@ -0,0 +1,36 @@
+package collection.set;
+
+import java.util.Arrays;
+
+public class HashStart4 {
+
+    static final int CAPACITY = 10;
+
+    public static void main(String[] args) {
+        System.out.println("hashIndex(1) = " + hashIndex(1));
+        System.out.println("hashIndex(14) = " + hashIndex(14));
+        System.out.println("hashIndex(99) = " + hashIndex(99));
+
+        Integer[] inputArray = new Integer[CAPACITY];
+        add(inputArray, 1);
+        add(inputArray, 14);
+        add(inputArray, 99);
+        System.out.println("inputArray = " + Arrays.toString(inputArray));
+
+        // 검색
+        int searchValue = 14;
+        int hashIndex = hashIndex(searchValue);
+        System.out.println("searchValue hashIndex = " + hashIndex);
+        Integer result = inputArray[hashIndex];
+        System.out.println(result);
+    }
+
+    private static void add(Integer[] inputArray, int value) {
+        int hashIndex = hashIndex(value);
+        inputArray[hashIndex] = value;
+    }
+
+    static int hashIndex(int value) {
+        return value % CAPACITY;
+    }
+}
